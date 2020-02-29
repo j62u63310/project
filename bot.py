596993003ef724c7032f944578c78ecfe0db1c7f
j62u63310 <61619167+j62u63310@users.noreply.github.com>
2020-03-01 01:23:06 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import json
+import random
 
 with open('setting.json','r',encoding='utf8') as jfile:
     jdata=json.load(jfile)
@@ -27,5 +28,29 @@ async def on_member_remove(member):
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)}毫秒')
+
+@bot.command()
+async def 大肥蛙(ctx):
+    random_pic=random.choice(jdata['大肥蛙'])
+    大肥蛙 = discord.File(random_pic)
+    await ctx.send(file=大肥蛙)
+
+@bot.command()
+async def 大鼻子(ctx):
+    random_pic=random.choice(jdata['大鼻子'])
+    大鼻子 = discord.File(random_pic)
+    await ctx.send(file=大鼻子)
+
+@bot.command()
+async def 阿蛙(ctx):
+    random_pic=random.choice(jdata['阿蛙'])
+    阿蛙 = discord.File(random_pic)
+    await ctx.send(file=阿蛙)
+
+@bot.command()
+async def 黑熊(ctx):
+    random_pic=random.choice(jdata['黑熊'])
+    黑熊 = discord.File(random_pic)
+    await ctx.send(file=黑熊)
 
 bot.run(jdata["Token"])
